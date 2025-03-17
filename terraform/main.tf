570bd12fac6edcf3dffead021b7b1d6d3b67b533
cloudcr0w <adamwronowy@gmail.com>
2025-03-17 15:22:08 +0800

    Fixed Terraform formatting issues in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,26 +12,26 @@ provider "aws" {
 }
 
 module "s3" {
-  source = "./modules/s3"
+  source      = "./modules/s3"
   bucket_name = "adamwrona-serverless-frontend"
 }
 
 module "dynamodb" {
-  source = "./modules/dynamodb"
+  source     = "./modules/dynamodb"
   table_name = "serverless-tasks"
 }
 
 module "iam" {
-  source = "./modules/iam"
+  source             = "./modules/iam"
   dynamodb_table_arn = module.dynamodb.dynamodb_table_arn
 }
 
 module "lambda" {
-  source = "./modules/lambda"
+  source          = "./modules/lambda"
   lambda_role_arn = module.iam.lambda_role_arn
 }
 
 module "apigateway" {
-  source = "./modules/apigateway"
+  source              = "./modules/apigateway"
   lambda_function_arn = module.lambda.lambda_function_arn
 }
