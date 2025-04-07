0c23fa8650a39934a487d0005a2fd36c3f3521f8
cloudcr0w <adamwronowy@gmail.com>
2025-04-07 16:32:48 +0800

    updated main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "backend_lambda" {
   runtime       = "python3.9"
   handler       = "lambda_function.lambda_handler"
   role          = var.lambda_role_arn
-  filename      = "lambda.zip"
+  filename      = "${path.module}/lambda.zip"
 
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 
@@ -12,9 +12,9 @@ resource "aws_lambda_function" "backend_lambda" {
     prevent_destroy = true
   }
   tags = {
-  Project     = "Serverless Task Manager"
-  Environment = "Dev"
-  Owner       = "Adam Wrona"
-}
+    Project     = "Serverless Task Manager"
+    Environment = "Dev"
+    Owner       = "Adam Wrona"
+  }
 
 }
