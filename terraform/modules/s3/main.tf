f2f2b11a649c110ec1e5fe281c14af0d90615a0a
Adam Wrona <adamwronowy@gmail.com>
2025-04-24 17:49:48 +0800

    updated code, added policy for s3

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -17,3 +17,21 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
     key = "error.html"
   }
 }
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = [
+          "s3:GetObject"
+        ]
+        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
