cde0daf92f1ccaaf4f22f56f02062f2c16aaa82c
Adam Wrona <adamwronowy@gmail.com>
2025-04-24 18:37:49 +0800

    fix: add public-read bucket policy for static frontend hosting

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -35,3 +35,12 @@ resource "aws_s3_bucket_policy" "public_read" {
     ]
   })
 }
+module "apigateway" {
+  source = "./modules/apigateway"
+  lambda_function_arn = module.lambda.lambda_arn
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  apigateway = module.apigateway
+}
