0104eb7b3b45d653e0c3b5c84d8ec1319d0391d5
Adam Wrona <adamwronowy@gmail.com>
2025-05-16 08:26:07 +0800

    fix: corrected lambda output and added alerting module

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,7 +1,8 @@
+
 output "lambda_function_arn" {
   value = aws_lambda_function.backend_lambda.arn
 }
 
 output "function_name" {
-  value = aws_lambda_function.api_lambda.function_name
+  value = aws_lambda_function.backend_lambda.function_name
 }
