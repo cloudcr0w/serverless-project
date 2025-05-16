e8b47f453a14181a18e2c52881aecebec1c6df9e
Adam Wrona <adamwronowy@gmail.com>
2025-05-16 16:05:14 +0800

    Fix IAM role wiring for Slack Lambda (ARN/name passed via module)

--- terraform/modules/slack_forwarder/main.tf
+++ terraform/modules/slack_forwarder/main.tf
@@ -1,17 +1,27 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "slack-forwarder-role"
-  assume_role_policy = jsonencode({
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "lambda-s3-access"
+  description = "Allow Lambda to get ZIP from S3"
+  policy      = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" },
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = "arn:aws:s3:::adamwrona-serverless-frontend/lambda/slack_alert_forwarder.zip"
+      }
+    ]
   })
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_s3_access_attachment" {
+  role       = var.lambda_role_name
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_logging" {
-  role       = aws_iam_role.lambda_role.name
+  role       = var.lambda_role_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -22,18 +32,17 @@ resource "aws_lambda_function" "slack_forwarder" {
   publish       = false
   handler       = "slack_alert_forwarder.lambda_handler"
   runtime       = "python3.10"
-  role          = aws_iam_role.lambda_role.arn
+  role          = var.lambda_role_arn
 
-    lifecycle {
+  lifecycle {
     ignore_changes = [source_code_hash]
   }
-  
+
   environment {
     variables = {
       SLACK_WEBHOOK_URL = var.slack_webhook_url
     }
   }
-
 }
 
 resource "aws_lambda_permission" "allow_sns" {
@@ -43,9 +52,3 @@ resource "aws_lambda_permission" "allow_sns" {
   principal     = "sns.amazonaws.com"
   source_arn    = var.sns_topic_arn
 }
-
-# resource "aws_sns_topic_subscription" "sns_to_lambda" {
-#   topic_arn = var.sns_topic_arn
-#   protocol  = "lambda"
-#   endpoint  = aws_lambda_function.slack_forwarder.arn
-# }
--- terraform/modules/slack_forwarder/variables.tf
+++ terraform/modules/slack_forwarder/variables.tf
@@ -1,9 +1,20 @@
-variable "sns_topic_arn" {
+variable "lambda_role_arn" {
+  description = "ARN of IAM role for the Slack forwarder Lambda"
   type        = string
-  description = "ARN of the SNS topic to subscribe to"
+}
+
+variable "lambda_role_name" {
+  description = "Name of the IAM role for the Lambda"
+  type        = string
 }
 
 variable "slack_webhook_url" {
+  description = "Slack Webhook URL for sending alerts"
   type        = string
-  description = "Slack webhook for URL alerts"
+  sensitive   = true
+}
+
+variable "sns_topic_arn" {
+  description = "ARN of SNS topic that triggers the Lambda"
+  type        = string
 }
