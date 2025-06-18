df6e51e7b0aa447a76fca82a871cfcb7250ffb6b
Adam Wrona <adamwronowy@gmail.com>
2025-06-18 08:29:51 +0800

    feat: add CloudWatch dashboard for serverless-backend Lambda

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,23 @@
+
+resource "aws_cloudwatch_dashboard" "lambda_dashboard" {
+  dashboard_name = "ServerlessBackend-Dashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type = "metric",
+        x = 0, y = 0, width = 24, height = 6,
+        properties = {
+          metrics = [
+            ["AWS/Lambda", "Invocations", "FunctionName", "serverless-backend"],
+            ["...", "Errors", "FunctionName", "serverless-backend"]
+          ],
+          period = 300,
+          stat = "Sum",
+          region = "us-east-1",
+          title = "Lambda Activity Overview"
+        }
+      }
+    ]
+  })
+}
