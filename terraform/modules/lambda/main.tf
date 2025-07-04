d6a1a733df121d8e0e8dbae104372677efa95eac
Adam Wrona <adamwronowy@gmail.com>
2025-07-04 12:44:38 +0800

    Add input validation for task title in POST requests

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,10 +1,10 @@
 
 resource "aws_lambda_function" "backend_lambda" {
-  function_name = "serverless-backend"
-  role          = var.lambda_role_arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  filename      = "${path.module}/lambda.zip"
+  function_name    = "serverless-backend"
+  role             = var.lambda_role_arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  filename         = "${path.module}/lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda.zip")
 }
 
@@ -13,5 +13,5 @@ resource "aws_lambda_permission" "allow_apigw_invoke" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.backend_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${var.api_execution_arn}/*/*"
+  source_arn    = "${var.api_execution_arn}/*/*"
 }
