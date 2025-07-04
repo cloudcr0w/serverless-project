a693984d8f4eba28b9e45212346488939aa801b4
Adam Wrona <adamwronowy@gmail.com>
2025-07-04 12:45:03 +0800

    Add S3 lifecycle rule to auto-delete Lambda ZIPs after 7 days

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket        = var.bucket_name
   force_destroy = true
-
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
 }
 
 resource "aws_s3_bucket_website_configuration" "frontend_website" {
@@ -13,25 +9,44 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
   index_document {
     suffix = "index.html"
   }
+
   error_document {
     key = "error.html"
   }
 }
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.frontend_bucket.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
         Action    = [
           "s3:GetObject"
-        ]
+        ],
         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "frontend_cleanup" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  rule {
+    id     = "cleanup"
+    status = "Enabled"
+
+    filter {
+      prefix = "lambda/"
+    }
+
+    expiration {
+      days = 7
+    }
+  }
 }
