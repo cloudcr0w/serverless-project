fcf2a965c921a8a5d3202a8905723613e626aed0
Adam Wrona <adamwronowy@gmail.com>
2025-07-10 21:36:38 +0800

    chore: disable public S3 bucket policy in preparation for CloudFront OAI

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -15,24 +15,24 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
   }
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.frontend_bucket.id
+# resource "aws_s3_bucket_policy" "public_read" {
+#   bucket = aws_s3_bucket.frontend_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = [
-          "s3:GetObject"
-        ],
-        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject",
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = [
+#           "s3:GetObject"
+#         ],
+#         Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_s3_bucket_lifecycle_configuration" "frontend_cleanup" {
   bucket = aws_s3_bucket.frontend_bucket.id
