18f24dc4ba316bdbde96da9185e4bda96159a320
Adam Wrona <adamwronowy@gmail.com>
2025-07-10 21:35:28 +0800

    feat: add CloudFront distribution with OAI for secure S3 access (disabled public policy)

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,68 @@
+resource "aws_cloudfront_origin_access_identity" "frontend_oai" {
+  comment = "OAI for accessing private S3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "frontend_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "s3-frontend-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.frontend_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-frontend-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "ServerlessFrontendCDN"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_secure_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect    = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.frontend_oai.iam_arn
+        },
+        Action    = ["s3:GetObject"],
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+      }
+    ]
+  })
+}
