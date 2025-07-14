c62d654cc6ca3f8e43ffb0769f6f8ebf8a398361
Adam Wrona <adamwronowy@gmail.com>
2025-07-14 14:19:29 +0800

    chore: add PUT method

--- terraform/utils.py
+++ terraform/utils.py
@@ -1,7 +1,7 @@
 COMMON_HEADERS = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
     "X-Content-Type-Options": "nosniff",
 }
