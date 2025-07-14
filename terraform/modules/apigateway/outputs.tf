34f6cf9c21d243cf6d634adead326559d3101349
Adam Wrona <adamwronowy@gmail.com>
2025-07-14 13:07:43 +0800

    feat(iac): add api_url output for simplified testing and curl usage

--- terraform/modules/apigateway/outputs.tf
+++ terraform/modules/apigateway/outputs.tf
@@ -1,3 +1,7 @@
 output "api_execution_arn" {
   value = aws_apigatewayv2_api.api_gateway.execution_arn
 }
+output "api_url" {
+  value = "https://${aws_apigatewayv2_api.api_gateway.id}.execute-api.${var.region}.amazonaws.com/${aws_apigatewayv2_stage.api_stage.name}"
+  description = "Full base URL for API Gateway"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "cloudfront_url" {
   value = "https://${aws_cloudfront_distribution.frontend_cdn.domain_name}"
 }
+output "api_url" {
+  value = module.apigateway.api_url
+}
