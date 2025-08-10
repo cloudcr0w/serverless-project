63e8bc0dda087bafe6dda31cc573820696821d69
Adam Wrona <adamwronowy@gmail.com>
2025-08-10 10:04:31 +0800

    test: add test for /tasks endpoint

--- flask-app/tests/test_app.py
+++ flask-app/tests/test_app.py
@@ -5,3 +5,10 @@ def test_healthcheck():
         response = client.get("/health")
         assert response.status_code == 200
         assert response.json == {"status": "ok"}
+
+def test_get_tasks():
+    with app.test_client() as client:
+        response = client.get("/tasks")
+        assert response.status_code == 200
+        assert isinstance(response.json, list)
+        assert all("task_id" in t for t in response.json)
