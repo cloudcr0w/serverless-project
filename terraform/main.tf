da057f56d034625d46ce4f72f9fe70cb2a25a1a6
Adam Wrona <adamwronowy@gmail.com>
2025-08-21 17:30:02 +0800

    Use module.lambda.lambda_function_name when wiring alerting module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+# Kto ja jestem (ID konta)
+data "aws_caller_identity" "current" {}
+
+# Jaki region z providera AWS
+data "aws_region" "current" {}
+
 terraform {
   backend "s3" {
     bucket  = "adamwrona-terraform-state"
@@ -47,7 +53,7 @@ module "lambda" {
 
 module "alerting" {
   source               = "./modules/alerting"
-  lambda_function_name = module.lambda.function_name
+  lambda_function_name = module.lambda.lambda_function_name
 }
 
 module "slack_forwarder" {
@@ -57,10 +63,18 @@ module "slack_forwarder" {
   sns_topic_arn     = module.alerting.sns_topic_arn
   slack_webhook_url = var.slack_webhook_url
 }
+# resource "aws_lambda_permission" "allow_apigw" {
+#   statement_id  = "AllowAPIGatewayInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = module.lambda.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${module.apigateway.execution_arn}/*/*"
+# }
 resource "aws_lambda_permission" "allow_apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda.function_name
+  function_name = module.lambda.lambda_function_name # <— to poprawka
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${module.apigateway.execution_arn}/*/*"
+
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${module.apigateway.api_id}/*/*"
 }
