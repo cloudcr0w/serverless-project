5e8c8cdf20b341ee435af101c4fc0e1e73ba8ade
Adam Wrona <adamwronowy@gmail.com>
2025-08-21 16:01:50 +0800

    Update API Gateway integration to reference passed Lambda ARN

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -5,7 +5,7 @@ resource "aws_apigatewayv2_api" "api_gateway" {
 
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["GET", "POST", "PUT", "DELETE"]
+    allow_methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     allow_headers = ["Content-Type"]
   }
 
@@ -14,11 +14,23 @@ resource "aws_apigatewayv2_api" "api_gateway" {
   # }
 }
 
+resource "aws_apigatewayv2_route" "health" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "GET /health"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.api_gateway.id
   integration_type       = "AWS_PROXY"
   integration_uri        = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
   payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "proxy_any" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "get_tasks_route" {
--- terraform/modules/apigateway/outputs.tf
+++ terraform/modules/apigateway/outputs.tf
@@ -6,3 +6,11 @@ output "api_url" {
   value       = "https://${aws_apigatewayv2_api.api_gateway.id}.execute-api.${var.region}.amazonaws.com/${aws_apigatewayv2_stage.api_stage.name}"
   description = "Full base URL for API Gateway"
 }
+# modules/apigateway/outputs.tf
+output "api_id" {
+  value = aws_apigatewayv2_api.api_gateway.id
+}
+
+output "invoke_url" {
+  value = aws_apigatewayv2_stage.api_stage.invoke_url
+}
